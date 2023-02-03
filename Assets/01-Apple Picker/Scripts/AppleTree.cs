1d5c7eccd2b1cd407350702b25e41325f8286882
dcdoan153 <123130804+dcdoan153@users.noreply.github.com>
2023-02-02 19:36:43 -0500

    Dropping Apples
    Apples drop and physics layer setup

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -23,8 +23,16 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         // Dropping apples every second
+        Invoke( "DropApple", 2f);
     }
 
+    void DropApple() 
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    
     // Update is called once per frame
     void Update()
     {
